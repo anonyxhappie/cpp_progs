05d921f91a763e6bc1449f52c2e6d9ee0de87e4e
Akshay Saini <akkilsl522@gmail.com>
2016-10-11 13:20:45 +0530

    Signed-off-by: Akshay Saini <akkilsl522@gmail.com>

--- spiralVector.cpp
+++ spiralVector.cpp
@@ -3,14 +3,14 @@
 #define tr(container, it) \
 		for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
 #define m 3
-#define n 3
+#define n 2
 using namespace std;
 class Solution{
 	public:
 		vector<int> spiralOrder(const vector<vector<int> >&);
 };
 int main(){
-	int data[][n] = {{1,2,3},{4,5,6},{7,8,9}};
+	int data[][n] = {{1,2},{3,4},{5,6}};
 	vector<vector<int> > A;
 	for(int i=0; i<m; i++) A.push_back(vector<int>());
 	for(int i=0; i<m; i++){
@@ -35,7 +35,7 @@ int main(){
 vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
 	vector<int> result;
 	int T, L, B, R, dir;
-	dir=T=L=0, B=R=A.size()-1;
+	dir=T=L=0, B=A.size()-1, R=A[0].size()-1;
 	while(T<=B && L<=R){
 		if(dir==0){
 			for(int i=L; i<=R; i++) result.push_back(A[T][i]);
